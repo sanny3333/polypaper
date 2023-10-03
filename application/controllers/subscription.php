029d93bfee6527a01346e2af6e4eb40488697308
kirtiraj16 <144887556+kirtiraj16@users.noreply.github.com>
2023-10-03 20:58:57 +0530

    add file in controller

---
+++ application/controllers/subscription.php
@@ -0,0 +1,37 @@
+// controllers/SubscriptionController.php
+
+class SubscriptionController extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('subscription_model');
+    }
+
+    public function index() {
+        $this->load->view('subscription_form');
+    }
+
+    public function subscribe() {
+        $email = $this->input->post('email');
+
+        // Validate email (you can add more validation)
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('subscription_form');
+        } else {
+            // Insert email into the database
+            $this->subscription_model->subscribe($email);
+
+            // Send confirmation email
+            $this->sendConfirmationEmail($email);
+
+            $data['message'] = 'Thank you for subscribing!';
+            $this->load->view('subscription_success', $data);
+        }
+    }
+
+    private function sendConfirmationEmail($email) {
+        // Implement code to send a confirmation email here
+        // You can use libraries like PHPMailer or built-in email functions
+    }
+}
